82b6ea937b8d41d0fbf4e102b88df3cc959806b9
Inixio <inixio97@gmail.com>
2021-02-04 22:59:53 +0100

    ADD classic who

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
 
-const episodes = require("./episodes");
+const newWho = require("./episodes");
+const classicWho = require("./serials");
 const token = process.env.TELEGRAM_TOKEN;
 
 console.log("[TARDIS console] Doctor Who Episodes Battle Bot launched!")
 
 const bot = new TelegramBot(token, {polling: true});
 
-bot.setMyCommands([
-    {
-        command: '/botwho',
-        description: 'Get a brief explanation'
-    }
-])
-
-bot.onText(/\/botwho/, (msg, match) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, `Hi! I'm a bot. I live into the TARDIS console. I generate random episodes battles to keep the Doctor's companions entertained\n\nWrite /battle to generate a random battle`);
-})
-
-bot.onText(/\/battle/, (msg, match) => {
+const getOptions = (category) => {
     let options = [];
+    let episodes = newWho;
+    if (category){
+        if (category === 'classic') {
+            episodes = classicWho;
+        } else if (category === 'all') {
+            episodes.concat(classicWho);
+        }
+    }
     for (let i = 0; i < 4; i++) {
         let episode = episodes[Math.floor(Math.random() * episodes.length)];
         while (options.find(o => o === episode)){
@@ -29,8 +26,59 @@ bot.onText(/\/battle/, (msg, match) => {
         }
         options.push(episode);
     }
-    bot.sendPoll(msg.chat.id, 'Dear companion, what adventure did you enjoy the most?', options, {
+    return options;
+}
+
+bot.setMyCommands([
+    {
+        command: '/botwho',
+        description: 'Get a brief explanation'
+    },
+    {
+        command: '/battle',
+        description: 'Generate random battle between modern Doctor Who episodes'
+    },
+    {
+        command: '/classicbattle',
+        description: 'Generate random battle between Classic Who serials'
+    },
+    {
+        command: '/timewar',
+        description: 'Generate random battle between Classic and New Who episodes'
+    }
+])
+
+bot.onText(/\/botwho/, (msg, match) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Hi! I\'m a bot. I live into the TARDIS console.\n' + 
+        'I generate random episodes battles to keep the Doctor\'s companions entertained.\n\n' + 
+        'Write /battle to generate a random battle between modern Doctor Who episodes\n\n' + 
+        'Write /classicbattle to generate a random battle between Classic Doctor Who serials\n\n' +
+        'Write /timewar to generate a random battle between any episode aired since 1963');
+})
+
+bot.onText(/\/battle/, (msg, match) => {
+    const options = getOptions()
+    bot.sendPoll(msg.chat.id, 'Dear companion, what adventure did you enjoy the most? #DoctorWhoBattle', options, {
         is_anonymous: false,
         allows_multiple_answers: false
     })
 })
+
+bot.onText(/\/classicbattle/, (msg, match) => {
+    const options = getOptions('classic')
+    bot.sendPoll(msg.chat.id, 'Dear companion, what classic adventure did you enjoy the most? #DoctorWhoBattle', options, {
+        is_anonymous: false,
+        allows_multiple_answers: false
+    })
+})
+
+bot.onText(/\/timewar/, (msg, match) => {
+    const options = getOptions('all')
+    bot.sendPoll(msg.chat.id, 'Dear companion, what adventure did you enjoy the most? #DoctorWhoBattle #TimeWar', options, {
+        is_anonymous: false,
+        allows_multiple_answers: false
+    })
+})
+
+bot.on('polling_error', (e) => console.log(e))
